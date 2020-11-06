66ac7abd00e828838dfa589380c0219329df06c9
Nataliapiesn <01123786@pw.edu.pl>
2020-11-06 07:05:17 +0000

    Task 1-2 with bugs: shadowed declaration.

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+using namespace std;
 
+class Wektor
+{
+public:
+    Wektor(int num_elements) { double* wektor = new double[num_elements]; }
+    ~Wektor() { delete[] wektor; }
+    void    print() { cout << wektor[4]; }
+    int     num_elements;
+    double* wektor = new double[num_elements];
+};
 int main()
 {
-    puts("Hello, World!");
+    Wektor A(5);
+    A.print();
 }
