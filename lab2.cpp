ae4b9d64e7d03c46e215d296399bf494d691aee9
Nataliapiesn <01123786@pw.edu.pl>
2020-11-06 08:06:32 +0000

    Fixed bug of shadowed delaration. Added member to class: dlugosc with a getter function. Task 3 completed.

--- lab2.cpp
+++ lab2.cpp
@@ -3,15 +3,25 @@ using namespace std;
 
 class Wektor
 {
+private:
+    int dlugosc;
+
 public:
-    Wektor(int num_elements) { double* wektor = new double[num_elements]; }
+    Wektor(int num_elements)
+    {
+        wektor  = new double[num_elements];
+        dlugosc = num_elements;
+    }
     ~Wektor() { delete[] wektor; }
-    void    print() { cout << wektor[4]; }
-    int     num_elements;
-    double* wektor = new double[num_elements];
+    void print() { cout << wektor[4] << "\n"; }
+
+    double* wektor;
+    int     get_dlugosc() { return dlugosc; }
 };
+
 int main()
 {
     Wektor A(5);
     A.print();
+    cout << A.get_dlugosc() << endl;
 }
