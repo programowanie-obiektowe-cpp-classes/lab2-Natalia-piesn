b6a12cbf43d98145d0561a989455fd335ec6d7d1
Nataliapiesn <01123786@pw.edu.pl>
2020-11-08 18:20:38 +0000

    Added method to Wektor class: changeLength(int). Task 6 completed.

--- lab2.cpp
+++ lab2.cpp
@@ -5,7 +5,7 @@ class Wektor
 {
 public:
     Wektor() { wektor = new double[0]; }
-    Wektor(int num_elements) : num_el{num_elements}, dlugosc{num_elements}, pojemnosc{num_elements}
+    Wektor(int num_elements) : num_el{num_elements}, length{num_elements}, capacity{num_elements}
     {
         wektor = new double[num_el];
         for (int i = 0; i < num_el; i++) {
@@ -16,24 +16,59 @@ public:
 
     void print()
     {
-        for (int i = 0; i < dlugosc; i++) {
-            cout << wektor[i] << "\n";
+        for (int i = 0; i < capacity; i++) {
+            cout << *(wektor + i) << "\n";
+        }
+    }
+
+    void changeLength(int new_length)
+    {
+        if (new_length <= capacity) {
+            length = new_length;
+            for (int i = length; i < capacity; i++) {
+                *(wektor + i) = 0.0;
+            }
+        }
+        else {
+            double* wektor_new = new double[new_length];
+            wektor_new         = wektor;
+            delete[] wektor;
+            for (int i = capacity; i < new_length; i++) {
+                *(wektor_new + i) = 0.0;
+            }
+            capacity = new_length;
+            length   = new_length;
         }
     }
 
     double* wektor;
-    int     num_el;
-    int     get_dlugosc() { return dlugosc; }
-    int     get_pojemnosc() { return pojemnosc; }
+    // double* wektor_new;
+    int num_el;
+    int get_length() { return length; }
+    int get_capacity() { return capacity; }
 
 private:
-    int dlugosc;
-    int pojemnosc;
+    int length;
+    int capacity;
 };
 
 int main()
 {
     Wektor A(10);
+    for (int i = 0; i < A.get_length(); i++) {
+        A.wektor[i] = i;
+    }
     A.print();
-    cout << A.get_dlugosc() << " " << A.get_pojemnosc() << endl;
+    cout << A.get_length() << " " << A.get_capacity() << endl;
+
+    A.changeLength(5);
+    A.print();
+    cout << A.get_length() << " " << A.get_capacity() << endl;
+
+    A.changeLength(20);
+    A.print();
+    cout << A.get_length() << " " << A.get_capacity() << endl;
+    A.changeLength(12);
+    A.print();
+    cout << A.get_length() << " " << A.get_capacity() << endl;
 }
