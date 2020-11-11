604a1f6b4cdad6cef051bb63704b3870aed5f3d8
Nataliapiesn <01123786@pw.edu.pl>
2020-11-11 17:43:54 +0000

    t1

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,5 +4,35 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    Resource res;
+
+    ResourceManager(const ResourceManager& res_man) : res{res_man.res} {};
+
+    ResourceManager(ResourceManager&& res_man) : res() { res = res_man.res; }
+
+    ~ResourceManager() {}
+
+    double get() { return res.get(); }
+
+    ResourceManager operator=(const ResourceManager& res_man)
+    {
+        if (&res_man == this) {
+            return *this;
+        }
+        else {
+            res = res_man.res;
+        }
+        return *this;
+    }
+
+    ResourceManager operator=(ResourceManager&& res_man)
+    {
+        if (&res_man == this) {
+            return *this;
+        }
+        else {
+            res = res_man.res;
+        }
+        return *this;
+    }
 };
